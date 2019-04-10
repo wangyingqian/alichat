6d075f8519e749025ee98a066dd0d9c433e4c24a
wangyingqian <shirohako@qq.com>
2019-04-10 09:55:08 +0800

    修改

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once 'vendor/autoload.php';
 
 use Wangyingqian\AliChat\AliChat;
 
-AliChat::alipay(['c'=>1])->fund('voucher', ['sss'=>5]);
+AliChat::alipay(['c'=>13])->fund('voucher', ['sss'=>5]);
